055781f8c1aff8feb309bacd7afffc0d73974dad
AliHerGuz <hernandez.guzman.ac@gmail.com>
2021-09-13 21:30:53 -0500

    Updated the icon

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ return `${day} ${hours}:${minutes}`;
 
 
 function displayTemperature(response){
-console.log(response.data);
+    console.log(response.data)
 
 let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
@@ -23,6 +23,9 @@ let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
 let windElement = document.querySelector("#wind");
 let dateElement = document.querySelector("#date");
+let maxElement = document.querySelector("#max");
+let minElement = document.querySelector("#min");
+let iconElement = document.querySelector("#icon");
 
 temperatureElement.innerHTML= Math.round(response.data.main.temp);
 cityElement.innerHTML=(response.data.name);
@@ -30,10 +33,15 @@ descriptionElement.innerHTML=(response.data.weather[0].main);
 humidityElement.innerHTML=(response.data.main.humidity);
 windElement.innerHTML=Math.round(response.data.wind.speed);
 dateElement.innerHTML=formatDate(response.data.dt*1000);
+maxElement.innerHTML=Math.round(response.data.main.temp_max);
+minElement.innerHTML=Math.round(response.data.main.temp_min);
+iconElement.setAttribute("src",`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "24a006e8f97a4f206afdea960907be08";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "Helsinki";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
+
